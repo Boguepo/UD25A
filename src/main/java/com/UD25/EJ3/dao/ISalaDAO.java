1a0c76f4b744234c38e27c7074cafa10ca92d64a
Boguepo <46709093+Boguepo@users.noreply.github.com>
2021-03-12 16:53:24 +0100

    Delete src directory

--- src/main/java/com/UD25/EJ3/dao/ISalaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.UD25.EJ3.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.UD25.EJ3.dto.Sala;
-
-public interface ISalaDAO extends JpaRepository<Sala, Integer> {
-
-}
